15e73cf1ba29b7f3bcd027ec23df94a24b961098
Victor <victor.badaro@fatec.sp.gov.br>
2022-01-11 10:20:25 -0300

    feat: add posts context and its hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { Header } from './components/Header';
+import { PostsProvider } from './contexts/PostsContext';
 
 import { CssBaseline } from '@mui/material';
 
 export function App() {
   return (
-    <>
+    <PostsProvider>
       <CssBaseline />
       <Header />
-    </>
+    </PostsProvider>
   );
 }
---
+++ src/contexts/PostsContext.tsx
@@ -0,0 +1,70 @@
+import { createContext, ReactNode, useEffect, useState } from 'react';
+import { api } from '../services/api';
+
+type PostData = {
+    id: number;
+    title: string;
+    body: string;
+    userId: number;
+}
+
+type Post = {
+    id: number;
+    postId: number;
+    title: string;
+    author: string | undefined;
+    body: string;
+    createdAt: string;
+}
+
+type User = {
+    id: number;
+    name: string;
+}
+
+interface PostsProviderProps {
+    children: ReactNode;
+}
+
+interface PostsContextProps {
+    posts: Post[];
+}
+
+const PostsContext = createContext<PostsContextProps>({} as PostsContextProps);
+
+function PostsProvider({ children }: PostsProviderProps) {
+    const [posts, setPosts] = useState<Post[]>([]);
+
+    useEffect(() => {
+        (async () => {
+            const postsResult = await api.get('/posts');
+            const usersResult = await api.get('/users');
+
+            const postsData: PostData[] = postsResult.data;
+            const usersData: User[] = usersResult.data;
+
+            const posts = postsData.map(post => {
+                const user: User | undefined = usersData.find(user => user.id === post.userId);
+
+                return {
+                    id: post.id,
+                    postId: post.id,
+                    title: post.title,
+                    author: user?.name,
+                    body: post.body,
+                    createdAt: `${new Date().toLocaleDateString('pt-BR')} ${new Date().toLocaleTimeString('pt-BR', { hour12: true, hour: '2-digit', minute: '2-digit', second: '2-digit' })}`
+                };
+            });
+
+            setPosts(posts);
+        })();
+    }, []);
+
+    return (
+        <PostsContext.Provider value={{ posts }}>
+            {children}
+        </PostsContext.Provider>
+    );
+}
+
+export { PostsContext, PostsProvider };
