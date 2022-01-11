ba0dcefe579cb468652ed19dbff47c0e4c5c0f2e
Victor <victor.badaro@fatec.sp.gov.br>
2022-01-11 10:33:11 -0300

    feat: add container to center the app content

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import { Header } from './components/Header';
 import { PostsProvider } from './contexts/PostsContext';
 
 import { CssBaseline } from '@mui/material';
+import { MainContainer } from './styles';
 
 export function App() {
   return (
     <PostsProvider>
       <CssBaseline />
       <Header />
+      <MainContainer>
+      </MainContainer>
     </PostsProvider>
   );
 }
---
+++ src/styles.ts
@@ -0,0 +1,6 @@
+import { styled } from '@mui/material';
+
+export const MainContainer = styled('main')(({ theme }) => ({
+    margin: '56px auto 0',
+    width: '80%'
+}));
