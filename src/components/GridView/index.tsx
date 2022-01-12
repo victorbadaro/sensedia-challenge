0190bdd00fb7b5150e665cb320550a1c86eefd3b
Victor <victor.badaro@fatec.sp.gov.br>
2022-01-12 14:23:36 -0300

    chore: add all the necessary components to grid view component

--- src/components/GridView/index.tsx
+++ src/components/GridView/index.tsx
@@ -1,5 +1,51 @@
+import { Grid, Card, Typography } from '@mui/material';
+import { ChangeEvent, useEffect, useState } from 'react';
+import { usePosts } from '../../hooks/usePosts';
+
+import { StyledCardContent, StyledFooter, StyledPagination } from './styles';
+
 export function GridView() {
+    const { posts } = usePosts();
+    const [postsToShow, setPostsToShow] = useState(posts.slice(0, 8));
+    const [page, setPage] = useState(1);
+
+    useEffect(() => {
+        setPostsToShow(posts.slice(0, 8));
+        setPage(1);
+    }, [posts]);
+
+    function handlePaginationChange(event: ChangeEvent<unknown>, value: number) {
+        const offset = 8 * (value - 1);
+
+        setPage(value);
+        setPostsToShow(posts.slice(offset, offset + 8));
+    }
+
     return (
-        <h1>Grid View</h1>
+        <>
+            <Grid container spacing={2}>
+                {postsToShow.map(post => (
+                    <Grid key={post.id} item xs={3}>
+                        <Card sx={{ height: '218px' }} variant='outlined'>
+                            <StyledCardContent>
+                                <Typography className='card-title' variant='h5'>{post.title}</Typography>
+                                <Typography className='card-info' color='text.secondary'>{post.author}</Typography>
+                                <Typography className='card-body' color='text.secondary'>{post.body}</Typography>
+                                <Typography className='card-footer' color='text.secondary'>{post.createdAt}</Typography>
+                            </StyledCardContent>
+                        </Card>
+                    </Grid>
+                ))}
+            </Grid>
+            <StyledFooter>
+                <span className='total-results'>TOTAL RESULTS {posts.length}</span>
+                <StyledPagination
+                    count={Math.ceil(posts.length / 8)}
+                    page={page}
+                    onChange={handlePaginationChange}
+                    color='secondary'
+                />
+            </StyledFooter>
+        </>
     );
 }
---
+++ src/components/GridView/styles.ts
@@ -0,0 +1,63 @@
+import { Box, CardContent, Pagination, styled } from '@mui/material';
+
+export const StyledCardContent = styled(CardContent)(({ theme }) => ({
+    '& .card-title': {
+        marginBottom: '8px',
+        fontSize: '16px',
+        fontWeight: 500,
+        lineHeight: '16px',
+        textTransform: 'capitalize',
+        textOverflow: 'ellipsis',
+        overflow: 'hidden',
+        whiteSpace: 'nowrap'
+    },
+    '& .card-info': {
+        marginBottom: '20px',
+        fontSize: '14px',
+        fontWeight: 400,
+        lineHeight: '16px',
+        color: '#6A6A6A'
+    },
+    '& .card-body': {
+        display: '-webkit-box',
+        marginBottom: '31px',
+        maxHeight: '80px',
+        fontSize: '14px',
+        fontWeight: 400,
+        lineHeight: '20px',
+        textOverflow: 'ellipsis',
+        overflow: 'hidden',
+        WebkitLineClamp: 4,
+        WebkitBoxOrient: 'vertical',
+        color: '#919191'
+    },
+    '& .card-footer': {
+        fontSize: '14px',
+        fontWeight: 400,
+        lineHeight: '16px',
+        color: '#6A6A6A'
+    }
+}));
+
+export const StyledFooter = styled(Box)(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginTop: '40px',
+    '& .total-results': {
+        fontSize: '14px',
+        fontWeight: 'bold',
+        color: '#6A6A6A'
+    }
+}));
+
+export const StyledPagination = styled(Pagination)(({ theme }) => ({
+    '& button': {
+        minWidth: '20px',
+        width: '20px',
+        height: '20px'
+    },
+    '& .css-19micn4-MuiButtonBase-root-MuiPaginationItem-root.Mui-selected': {
+        backgroundColor: '#5C21AA'
+    }
+}));
